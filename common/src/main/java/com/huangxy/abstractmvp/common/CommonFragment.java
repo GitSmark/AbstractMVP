a6f0f2b2fdd11170b9c4cf601848f2617d89acc4
huangxy <woaini8143@139.com>
2021-02-22 23:57:48 +0800

    AbstractMVP 2.0 Fix

--- common/src/main/java/com/huangxy/abstractmvp/common/CommonFragment.java
+++ common/src/main/java/com/huangxy/abstractmvp/common/CommonFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import androidx.annotation.IdRes;
 import androidx.annotation.Nullable;
 
 import java.util.List;
